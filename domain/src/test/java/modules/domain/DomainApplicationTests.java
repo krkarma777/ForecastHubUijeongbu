b968ef94ac5f174fabee9f9aaef02b3d9e74c4b0
krkarma777 <krkarma777@gmail.com>
2024-04-28 00:25:58 +0900

    도메인 모듈 추가
    프로젝트의 멀티모듈 구조 확장을 위해 domain 모듈을 추가하였습니다. 이 모듈은 비즈니스 로직과 도메인 모델을 포함하여, 프로젝트의 핵심 기능을 관리합니다.

---
+++ domain/src/test/java/modules/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package modules.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
