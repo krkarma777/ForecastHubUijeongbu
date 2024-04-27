b41a1f06b936030da96a76e7f364dd65165e137e
krkarma777 <krkarma777@gmail.com>
2024-04-28 00:12:00 +0900

    Initial commit

---
+++ src/main/java/com/forecasthubuijeongbu/ForecastHubUijeongbuApplication.java
@@ -0,0 +1,13 @@
+package com.forecasthubuijeongbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastHubUijeongbuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForecastHubUijeongbuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forecasthubuijeongbu/ForecastHubUijeongbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forecasthubuijeongbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastHubUijeongbuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
