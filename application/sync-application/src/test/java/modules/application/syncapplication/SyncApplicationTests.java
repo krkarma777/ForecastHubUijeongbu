99442573d8b87d31bc9b544b702337e76af6f789
krkarma777 <krkarma777@gmail.com>
2024-04-28 18:24:41 +0900

    sync-application 모듈 추가
    - 외부 API (예: 기상청 API)와의 동기화를 담당하는 모듈

---
+++ application/sync-application/src/test/java/modules/application/syncapplication/SyncApplicationTests.java
@@ -0,0 +1,13 @@
+package modules.application.syncapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
