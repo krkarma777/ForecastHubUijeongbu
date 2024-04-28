ef731a94a185dd508eab30724693870897fe9205
krkarma777 <krkarma777@gmail.com>
2024-04-28 18:25:44 +0900

    inquire-application 모듈 추가
    - 데이터 조회를 담당하는 모듈

---
+++ application/inquire-application/src/main/java/modules/application/inquireapplication/InquireApplication.java
@@ -0,0 +1,13 @@
+package modules.application.inquireapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InquireApplication.class, args);
+    }
+
+}
---
+++ application/inquire-application/src/test/java/modules/application/inquireapplication/InquireApplicationTests.java
@@ -0,0 +1,13 @@
+package modules.application.inquireapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
