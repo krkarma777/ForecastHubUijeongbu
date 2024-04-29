df35739ec3fbb2d37dbe0c1f8e2c093fb4d0d7d9
krkarma777 <krkarma777@gmail.com>
2024-04-29 10:13:46 +0900

    ForecastHubUijeongbuApplication에 각 모듈의 컴포넌트스캔 어노테이션 추가

--- src/main/java/com/forecasthubuijeongbu/ForecastHubUijeongbuApplication.java
+++ src/main/java/com/forecasthubuijeongbu/ForecastHubUijeongbuApplication.java
@@ -3,9 +3,11 @@ package com.forecasthubuijeongbu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"modules.domain.entity"})
+@ComponentScan(basePackages = {"com.forecasthubuijeongbu", "modules"})
 public class ForecastHubUijeongbuApplication {
 
     public static void main(String[] args) {
