149f77703a517f7ee239e9144e459ad1447d303c
krkarma777 <krkarma777@gmail.com>
2024-04-29 10:11:12 +0900

    단기예보 요청을 위한 WeatherForecastRequestDTO 클래스 생성

--- domain/src/main/java/modules/domain/dto/WeatherForecastRequestDTO.java
+++ domain/src/main/java/modules/domain/dto/WeatherForecastRequestDTO.java
@@ -1,17 +1,67 @@
 package modules.domain.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class WeatherForecastRequestDTO {
-    private String serviceKey;          // 공공데이터포털에서 받은 인증키
     private int pageNo;                 // 페이지 번호
     private int numOfRows;              // 한 페이지 결과 수
     private String dataType = "JSON";   // 응답 자료 형식 (XML/JSON)
     private String baseDate;            // 발표일자
     private String baseTime;            // 발표시각
-    private int nx;                     // 예보지점 X 좌표
-    private int ny;                     // 예보지점 Y 좌표
+    private Double nx;                     // 예보지점 X 좌표
+    private Double ny;                     // 예보지점 Y 좌표
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getNumOfRows() {
+        return numOfRows;
+    }
+
+    public void setNumOfRows(int numOfRows) {
+        this.numOfRows = numOfRows;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public String getBaseDate() {
+        return baseDate;
+    }
+
+    public void setBaseDate(String baseDate) {
+        this.baseDate = baseDate;
+    }
+
+    public String getBaseTime() {
+        return baseTime;
+    }
+
+    public void setBaseTime(String baseTime) {
+        this.baseTime = baseTime;
+    }
+
+    public Double getNx() {
+        return nx;
+    }
+
+    public void setNx(Double nx) {
+        this.nx = nx;
+    }
+
+    public Double getNy() {
+        return ny;
+    }
+
+    public void setNy(Double ny) {
+        this.ny = ny;
+    }
 }
