4766c65fe9f0a3494e7184135507c50c9eb1de75
krkarma777 <krkarma777@gmail.com>
2024-04-29 09:46:34 +0900

    재사용 될 수 있는 공통 로직을 처리하기위한 common 모듈 추가

---
+++ common/src/test/java/modules/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package modules.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
