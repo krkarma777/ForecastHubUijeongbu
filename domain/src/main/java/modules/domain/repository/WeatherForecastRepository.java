508bcd3fd34ab9e2446bb1716908bee5f7c0498b
krkarma777 <krkarma777@gmail.com>
2024-04-30 10:29:44 +0900

    WeatherForecastRepository 주석 추가

--- domain/src/main/java/modules/domain/repository/WeatherForecastRepository.java
+++ domain/src/main/java/modules/domain/repository/WeatherForecastRepository.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+// 날씨 예보 레포지토리 인터페이스
 @Repository
 public interface WeatherForecastRepository extends JpaRepository<WeatherForecast, Long> {
+    // x, y 좌표로 날씨 예보 조회
     List<WeatherForecast> findByNxAndNy(int nx, int ny);
+
+    // 주어진 기준 날짜, 시간, x, y 좌표 및 카테고리로 날씨 예보가 존재하는지 확인
     boolean existsByBaseDateAndBaseTimeAndNxAndNyAndCategory(String baseDate, String baseTime, int nx, int ny, String category);
 }
